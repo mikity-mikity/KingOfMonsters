d7abed8b95f51e3f591b23a4f557f7254463e1d1
masaaki <masaaki@mikity.jp.net>
2023-02-07 20:24:20 +0900

    gg

--- eigenwrapper/eigenwrapper.cpp
+++ eigenwrapper/eigenwrapper.cpp
@@ -3061,7 +3061,8 @@ void KingOfMonsters::_mySparse::turnDense()
 std::string KingOfMonsters::_mySparse::_solveLU_sparse_cpu(Eigen::VectorXd* rhs, Eigen::VectorXd* ret)
 {
 	this->_mat[0].makeCompressed();
-	Eigen::PardisoLU < Eigen::SparseMatrix<double, 0, int64_t>> lu;
+	Eigen::SparseLU< Eigen::SparseMatrix<double, 0, int64_t>> lu;
+	//Eigen::PardisoLU < Eigen::SparseMatrix<double, 0, int64_t>> lu;
 	lu.compute(this->_mat[0]);
 	if (lu.info() == Eigen::ComputationInfo::Success)
 	{
